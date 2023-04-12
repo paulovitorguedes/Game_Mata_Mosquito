ad34a6471244f19112c74cd33232b0f91dc368b9
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2023-04-12 14:30:22 -0300

    function ajustarTamanhoPalcoJogo
    Busca o tamanho atual da tela html para que o jogo se ajuste com a janela

---
+++ _js/script.js
@@ -0,0 +1,13 @@
+var altura = 0;
+var largura = 0;
+
+// Mede a altura e largura da tela html
+function ajustarTamanhoPalcoJogo() {
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+
+    console.log(altura, largura);
+}
+
+ajustarTamanhoPalcoJogo();
+
