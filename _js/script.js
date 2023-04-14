54d17db2417f7f8382e6c86680d8803234b1c0bd
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2023-04-13 23:00:50 -0300

    Mosquito ramdom scaleX
    Function randonMirror()
    Função cria um espelho da imagem mosquito

--- _js/script.js
+++ _js/script.js
@@ -1,46 +1,66 @@
 var altura = 0;
 var largura = 0;
 
+ajustarTamanhoPalcoJogo();
+randomPosition();
+
+
 // Mede a altura e largura da tela html
 function ajustarTamanhoPalcoJogo() {
     largura = window.innerWidth;
     altura = window.innerHeight;
 }
-ajustarTamanhoPalcoJogo();
 
-// Math.random busca um valor randomico
-// Math.floor aredonda o vaalor
-// Caso a posição da imagem cair próximo ao canto direito da tela, o comprimento da imagem irá passar da tela e criando uma barra de rolagem
-// Neste cado subtraimos uma valor em pixel superior a largura da imagem
-var positionX = Math.floor(Math.random() * largura) - 90;
-var positionY = Math.floor(Math.random() * altura) - 90;
-positionX = positionX < 0 ? 0 : positionX;
-positionY = positionY < 0 ? 0 : positionY;
 
-// cria um document img
-var mosquito = document.createElement('img');
-mosquito.src = '_img/mosquito.png';
-mosquito.className = tamanhoAleatorio();
-mosquito.style.left = positionX + 'px';
-mosquito.style.top = positionY + 'px';
-mosquito.style.position = 'absolute';
+// Muda de forma randômica a posição do mosquito
+function randomPosition() {
+    // Math.random busca um valor randomico
+    // Math.floor aredonda o vaalor
+    // Caso a posição da imagem cair próximo ao canto direito da tela, o comprimento da imagem irá passar da tela e criando uma barra de rolagem
+    // Neste cado subtraimos uma valor em pixel superior a largura da imagem
+    var positionX = Math.floor(Math.random() * largura) - 90;
+    var positionY = Math.floor(Math.random() * altura) - 90;
+    positionX = positionX < 0 ? 0 : positionX;
+    positionY = positionY < 0 ? 0 : positionY;
 
-// cria um filho para o body
-document.body.appendChild(mosquito);
-console.log(tamanhoAleatorio());
+    // cria um document img
+    var mosquito = document.createElement('img');
+    mosquito.src = '_img/mosquito.png';
+    mosquito.className = randonSize() + " " + randonMirror();
+    mosquito.style.left = positionX + 'px';
+    mosquito.style.top = positionY + 'px';
+    mosquito.style.position = 'absolute';
 
-function tamanhoAleatorio() {
+    // cria um filho para o body
+    document.body.appendChild(mosquito);
+    console.log(randonMirror())
+}
+
+
+// Muda de forma randômica o tamanho do mosquito
+function randonSize() {
     var classe = Math.floor(Math.random() * 3);
 
     switch (classe) {
         case 0:
             return 'mosquito1';
-            break;
         case 1:
             return 'mosquito2';
-            break;
         case 2:
             return 'mosquito3';
-            break;
+    }
+}
+
+
+
+// Muda de forma randômica o lado do mosquito, espelhando a imagem
+function randonMirror() {
+    var classe = Math.floor(Math.random() * 2);
+
+    switch (classe) {
+        case 0:
+            return 'ladoA';
+        case 1:
+            return 'ladoB';
     }
 }
