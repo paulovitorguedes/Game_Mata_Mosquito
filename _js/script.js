7a75b4bf42fe223b30b1cfeadabc82a4f1af9e05
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2023-04-14 12:24:53 -0300

    Raising and Removing Mosquitoes
    -Criando e removendo mosquitos a cada ciclo de tempo
    -Add element id 'mosquito'

--- _js/script.js
+++ _js/script.js
@@ -4,6 +4,9 @@ var largura = 0;
 ajustarTamanhoPalcoJogo();
 randomPosition();
 
+// o comando roda a function randomPosition() a cada 1000ms (1 segundo)
+setInterval(function () { randomPosition(); }, 1000);
+
 
 // Mede a altura e largura da tela html
 function ajustarTamanhoPalcoJogo() {
@@ -14,6 +17,14 @@ function ajustarTamanhoPalcoJogo() {
 
 // Muda de forma randômica a posição do mosquito
 function randomPosition() {
+
+    // Remover o mosquito anterior (caso exista)
+    // Caso exista o elemento mosquito, o retorno é Thue, caso negativo retorna null
+    if (document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove();
+    }
+
+
     // Math.random busca um valor randomico
     // Math.floor aredonda o vaalor
     // Caso a posição da imagem cair próximo ao canto direito da tela, o comprimento da imagem irá passar da tela e criando uma barra de rolagem
@@ -30,6 +41,7 @@ function randomPosition() {
     mosquito.style.left = positionX + 'px';
     mosquito.style.top = positionY + 'px';
     mosquito.style.position = 'absolute';
+    mosquito.id = 'mosquito';
 
     // cria um filho para o body
     document.body.appendChild(mosquito);
