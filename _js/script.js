3e38af0aaaa617aeb400c55726be2bda19544e1d
Paulo Vitor Guedes <vitor_cguedes@outlook.com>
2023-04-13 21:42:01 -0300

    Mosquito random position
    Adicionando posições randômicas da imagem mosquito

--- _js/script.js
+++ _js/script.js
@@ -3,11 +3,28 @@ var largura = 0;
 
 // Mede a altura e largura da tela html
 function ajustarTamanhoPalcoJogo() {
-    altura = window.innerHeight;
     largura = window.innerWidth;
-
-    console.log(altura, largura);
+    altura = window.innerHeight;
 }
-
 ajustarTamanhoPalcoJogo();
 
+// Math.random busca um valor randomico
+// Math.floor aredonda o vaalor
+// Caso a posição da imagem cair próximo ao canto direito da tela, o comprimento da imagem irá passar da tela e criando uma barra de rolagem
+// Neste cado subtraimos uma valor em pixel superior a largura da imagem
+var positionX = Math.floor(Math.random() * largura) - 90;
+var positionY = Math.floor(Math.random() * altura) - 90;
+positionX = positionX < 0 ? 0 : positionX;
+positionY = positionY < 0 ? 0 : positionY;
+
+// cria um document img
+var mosquito = document.createElement('img');
+mosquito.src = '_img/mosquito.png';
+mosquito.className = 'mosquito1';
+mosquito.style.left = positionX + 'px';
+mosquito.style.top = positionY + 'px';
+mosquito.style.position = 'absolute';
+
+// cria um filho para o body
+document.body.appendChild(mosquito)
+console.log(positionX)
